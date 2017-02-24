1fa3aba136d1a79388e6711089e5c4b148681dfa
Ilya Dubov <pikkvile@gmail.com>
2017-02-25 01:49:26 +0300

    authorization complete

--- src/event_summary/consumer.clj
+++ src/event_summary/consumer.clj
@@ -10,13 +10,15 @@
 (def cs (cookies/cookie-store))
 
 (def cfg {:cookie-store cs :throw-exceptions? false})
-(def creds {:body
-  "[\"^ \", \"~:email\", \"Ilya@accrue.com\", \"~:password\", \"password\"]"
-  :headers {"Content-Type" "application/transit+json"}})
+(def tjson {"Content-Type" "application/transit+json"})
+(def host-referer {"Host" "169.45.250.98:3000" "Referer" "http://169.45.250.98:3000/dashboard"})
+(def creds {:body "[\"^ \", \"~:email\", \"Ilya@accrue.com\", \"~:password\", \"password\"]"})
 
-(defn authorize [] (
+(def tokens {:csrf nil :auth nil})
+(defn authorize []
   (client/get dashboard cfg)
-  (client/post login (merge cfg creds))
-  (client/get prepare (merge cfg {:headers {
-    "Host" "169.45.250.98:3000"
-    "Referer" "http://169.45.250.98:3000/dashboard"}}))))
+  (client/post login (merge cfg creds {:headers tjson}))
+  (let [csrf-token (((client/get prepare (merge cfg {:as :json :headers host-referer})) :body) :csrf-token)]
+    (let [auth-token (((client/post login (merge cfg creds {:as :json
+      :headers (merge tjson {"X-CSRF-Token" csrf-token})})) :body) :auth-token)]
+      (assoc tokens :csrf csrf-token :auth auth-token))))
