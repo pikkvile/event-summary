a53ae8645247037e96e12aa0c89d4f17c3840e75
Ilya Dubov <pikkvile@gmail.com>
2017-03-06 20:45:38 +0300

    Cells highlighting

--- src/event_summary/xlsx.clj
+++ src/event_summary/xlsx.clj
@@ -21,7 +21,17 @@
   (println "Extracting tickers from xlsx...")
   (flat-tickers (read-tickers)))
 
-(defn generate-output [event-id data] ; TODO: colors, fonts, rounding
+(def highlight-after 70)
+(defn need-highlight [cell]
+  (and
+    (or
+      (= 1 (.getColumn (.getAddress cell)))
+      (= 4 (.getColumn (.getAddress cell)))
+      (= 7 (.getColumn (.getAddress cell)))
+      (= 10 (.getColumn (.getAddress cell)))
+      (= 13 (.getColumn (.getAddress cell))))
+    (>= (.getNumericCellValue cell) highlight-after)))
+(defn generate-output [event-id data]
   (let [wb (create-workbook "Event Summary"
                             (into [["Symbol" "Bars 1-5" "Avg Peak Higher" "Avg Peak Lower"
                                         "Bars 6-10" "Avg Peak Higher" "Avg Peak Lower"
@@ -29,8 +39,13 @@
                                         "Bars 16-20" "Avg Peak Higher" "Avg Peak Lower"
                                         "Bars 21+" "Avg Peak Higher" "Avg Peak Lower"]] data))
         sheet (select-sheet "Event Summary" wb)
+        bold (create-cell-style! wb {:font {:bold true}})
+        yellow-highlight (create-cell-style! wb {:background :yellow})
+        yellow-bold (create-cell-style! wb {:background :yellow :font {:bold true}})
         header-row (first (row-seq sheet))]
     (do
-      (set-row-style! header-row (create-cell-style! wb {:font {:bold true}}))
+      (set-row-style! header-row bold)
+      (run! #(set-cell-style! % yellow-bold) (filter #(.contains (.getStringCellValue %) "Bars") (cell-seq header-row)))
+      (run! #(set-cell-style! % yellow-highlight) (filter need-highlight (flatten (map cell-seq (rest (row-seq sheet))))))
       (save-workbook! (str "/tmp/event-summary-" event-id ".xlsx") wb)))
   )
